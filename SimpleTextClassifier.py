e5ce1e283c2d7a087d93e41bb211495eb4dcab23
Aparna <vishnusreekumar58@gmail.com.comexit()git config --global user.email you@example.com>
2018-09-26 15:18:01 +0530

    files added

--- SimpleTextClassifier.py
+++ SimpleTextClassifier.py
@@ -11,7 +11,7 @@ news = fetch_20newsgroups(subset='all')
 def model(classifier, X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     classifier.fit(X_train, y_train)
-	predicted = classifier.predict(X_test)
+    predicted = classifier.predict(X_test)
     accuracy = accuracy_score(y_test, predicted)
     return classifier, accuracy
 
