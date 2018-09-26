b429fd9a12af3b3a2cc2cc8e3716ff715db23ab2
Aparna <vishnusreekumar58@gmail.com.comexit()git config --global user.email you@example.com>
2018-09-26 15:15:32 +0530

    files added

--- SimpleTextClassifier.py
+++ SimpleTextClassifier.py
@@ -4,13 +4,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
 
 news = fetch_20newsgroups(subset='all')
 
 def model(classifier, X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     classifier.fit(X_train, y_train)
-    accuracy = classifier.score(X_test, y_test)
+	predicted = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, predicted)
     return classifier, accuracy
 
 pipeline = Pipeline([('vectorizer', TfidfVectorizer()), 
